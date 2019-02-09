3d78e8f0d027bc85ba2942480ff3e8e7c01088b0
Sander in 't Hout <sander@inthout.eu>
2019-02-09 17:24:16 +0100

    reorganisation and updating libs
    - move source files into the src directory
    - update Blazor to latest version

---
+++ src/Blazor.LoadingIndicator/IRunningTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazor.LoadingIndicator
+{
+    public interface IRunningTask : IDisposable
+    {
+        double? Progress { get; set; }
+
+        string Maintext { get; set; }
+
+        string Subtext { get; set; }
+    }
+}
