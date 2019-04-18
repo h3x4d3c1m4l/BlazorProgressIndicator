9bd9981577925163f98298283b6207878e65f57d
Sander in 't Hout <sander@inthout.eu>
2019-04-18 22:09:08 +0200

    upgrade to Blazor 0.9.0

--- src/Blazor.LoadingIndicator.Sample.Server/Startup.cs
+++ src/Blazor.LoadingIndicator.Sample.Server/Startup.cs
@@ -20,14 +20,9 @@ namespace Blazor.LoadingIndicator.Sample.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseResponseCompression();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
             app.UseMvc(routes =>
             {
